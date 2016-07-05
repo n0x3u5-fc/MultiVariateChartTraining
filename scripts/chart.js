f563334eef1ca90644fd287de9358c302599240b
n0x3u5-fc <nakshatra@fusioncharts.com>
2016-07-06 00:54:47 +0530

    Simplified user JSON, encapsulated AJAX functions

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,24 +1,25 @@
 ;(function(window){
     'use strict';
-    var httpRequest;
-    var makeRequest = function(url) { 
-        httpRequest = new XMLHttpRequest();
-        if(!httpRequest) {
+    var AJAX = function(url, callback) {
+        this.httpRequest = new XMLHttpRequest;
+        if(!this.httpRequest) {
             console.log("Unable to create XMLHTTP instance.");
             return false;
         }
-        httpRequest.onreadystatechange = alertContents;
-        httpRequest.open('POST', url, true);
-        httpRequest.send();
-    };
-    var alertContents = function() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if(httpRequest.status === 200) {
-                parseData(httpRequest.responseText);
-            } else {
-                console.log("There was a problem with the request");
+        this.httpRequest.open('POST', url, true);
+        this.httpRequest.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE) {
+                if(this.status === 200) {
+                    callback(JSON.parse(this.responseText));
+                } else {
+                    console.log("There was a problem with the request");
+                }
             }
-        }
+        };
+        this.httpRequest.send();
     };
-    makeRequest('res/data/user_data.json');
+    var parseData = function(json) {
+        console.log(json);
+    };
+    var ajax = new AJAX('res/data/user_data.json', parseData);
 })(window);
