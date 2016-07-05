bea1f6d966032dc7109e57227ca58b61c3ae8bf2
n0x3u5-fc <nakshatra@fusioncharts.com>
2016-07-05 19:30:28 +0530

    Loaded JSON via AJAX

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,1 +1,24 @@
-console.log("Hello, Jupiter!");
+;(function(window){
+    'use strict';
+    var httpRequest;
+    var makeRequest = function(url) { 
+        httpRequest = new XMLHttpRequest();
+        if(!httpRequest) {
+            console.log("Unable to create XMLHTTP instance.");
+            return false;
+        }
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open('POST', url, true);
+        httpRequest.send();
+    };
+    var alertContents = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if(httpRequest.status === 200) {
+                parseData(httpRequest.responseText);
+            } else {
+                console.log("There was a problem with the request");
+            }
+        }
+    };
+    makeRequest('res/data/user_data.json');
+})(window);
